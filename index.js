df0bc75c9a5c82c4481a33b04b6508723077441d
Hannah Bradley <hannahthesailor@yahoo.com>
2024-01-23 15:34:13 -0500

    updated functions and renturns

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a,b) {
+    return a + b
+  }
+  function increment(n) {
+    return n + 1;
+  }
+
+  function subtract(a,b){
+    return a - b
+  }
+function decrement(n) {
+  return n - 1;
+}
+
+function multiply(a, b) {
+    return a * b;
+  }
+  
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  function divide(a,b){
+    return a / b
+  }  
+  function preserveDecimal(n) {
+    return parseFloat(n);
+}
